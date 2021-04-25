573d77e9cbcae458f3e71da1800ae793f5ffe610
kvtna <kvtna@192.168.0.6>
2021-04-26 00:31:13 +0530

    cancel commit

--- PSIC/src/main/java/com/Project/PSIC/Repository/CancelledRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Project.PSIC.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Project.PSIC.Model.Cancelled;
-
-public interface CancelledRepository extends JpaRepository<Cancelled,String>{
-	public Cancelled findByPemail(String email); 
-}
