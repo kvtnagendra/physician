b5548cc71bf87fce9392821b51593cbd1265437c
kvtna <kvtna@192.168.0.6>
2021-04-26 00:30:18 +0530

    admin commit

--- PSIC/src/main/java/com/Project/PSIC/Repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Project.PSIC.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Project.PSIC.Model.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin,String>{
-	public Admin findByEmail(String email); 
-}
